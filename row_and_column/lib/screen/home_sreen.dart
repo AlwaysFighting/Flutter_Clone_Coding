d8b400e9841bce5f6a1c301de5a6a2e53186536b
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-01-17 02:59:32 +0900

    add row_and_column Project File

---
+++ row_and_column/lib/screen/home_sreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            // CrossAxisAlignment - 반대축 정렬
+            crossAxisAlignment : CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.red,
+                width:50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 50.0,
+                height: 50.0,
+              ), Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
