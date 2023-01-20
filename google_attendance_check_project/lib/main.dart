992c6c5c299199523337fec68c57214f5c14d232
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-01-20 23:18:00 +0900

    feat : Google Attendence Check Project Starts

---
+++ google_attendance_check_project/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_attendance_check_project/screen/home_screen.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: HomeScreen()
+      )
+  );
+}
