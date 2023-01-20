07f83b62fac62ac82581e7c5a3ba738981cc86a7
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-01-20 19:25:18 +0900

    Push & Pop 의 여러 가지 기능을 공부함. 가령, canPop(), maybePop, pushReplacement, pushNamedAndRemoveUntil 같은 것들?

--- note_practice/lib/screen/home_screen.dart
+++ note_practice/lib/screen/home_screen.dart
@@ -7,18 +7,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(title: "HomeScreen", children: [
-      ElevatedButton(
-          onPressed: () async {
-           final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                  builder: (BuildContext context) => RouteOneScreen(
-                    number: 123,
-                  )),
-            );
-           print(result);
-          },
-          child: Text("Push!"))
-    ]);
+    return WillPopScope(
+      // onWillPop 은 async 를 붙여야 한다 ❗️
+      onWillPop: () async {
+        // true - pop 가능
+        // false - pop 불가능
+        final canPop = Navigator.of(context).canPop();
+        return canPop;
+     },
+      child: MainLayout(title: "HomeScreen", children: [
+        ElevatedButton(onPressed: (){
+          Navigator.of(context).canPop();
+        }, child: Text("can pop")),
+        ElevatedButton(
+            onPressed: (){
+              // 더이상 네비게이션을 pop 할 수 없을 때 뒤로가기 버튼을 제한하는 기능
+              Navigator.of(context).maybePop();
+            },
+            child: Text("maybePop")),
+        ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            child: Text("POP")),
+        ElevatedButton(
+            onPressed: () async {
+             final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (BuildContext context) => RouteOneScreen(
+                      number: 123,
+                    )),
+              );
+             print(result);
+            },
+            child: Text("Push!"))
+      ]),
+    );
   }
 }
