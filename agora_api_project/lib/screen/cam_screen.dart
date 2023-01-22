75133481e054bcc6d4162235413a3c2e753fb8f0
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-01-23 02:58:36 +0900

    feat : 카메라, 마이크 권한 요청까지 완료

---
+++ agora_api_project/lib/screen/cam_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class CamScreen extends StatelessWidget {
+  const CamScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LIVE"),
+      ),
+      body: FutureBuilder<bool>(
+        future: init(),
+        builder: (context, snapshot) {
+          if(snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+
+          if(!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Column(
+            children: [
+
+            ],
+          );
+        }
+      ),
+    );
+  }
+
+  Future<bool> init() async {
+    // 최종 응답 데이터 받아오기
+    final resp = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = resp[Permission.camera];
+    final mircrophonePermission = resp[Permission.microphone];
+
+    // cameraPermission 이 권한이 없다면 에러를 던지겠다.
+    if(cameraPermission != PermissionStatus.granted ||
+        mircrophonePermission != PermissionStatus.granted ) {
+      throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    return true;
+  }
+
+}
--- agora_api_project/lib/screen/home_screen.dart
+++ agora_api_project/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:agora_api_project/screen/cam_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -26,23 +27,44 @@ class _Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          Icons.videocam,
-          color: Colors.white,
-          size: 40.0,
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300]!,
+              // 흐린 임팩트가 얼마나 넓게 퍼질 것인지
+              blurRadius:   12.0,
+              // 그림자가 얼마나 퍼질건지
+              spreadRadius: 2.0
+            )
+          ]
         ),
-        SizedBox(width: 12.0,),
-        Text(
-          'Live',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30.0,
-            //letterSpacing:
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.videocam,
+                color: Colors.white,
+                size: 40.0,
+              ),
+              SizedBox(width: 12.0,),
+              Text(
+                'LIVE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  letterSpacing: 4.0
+                ),
+              )
+            ],
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }
@@ -52,7 +74,11 @@ class _Image extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Image.asset(
+        'asset/img/home_img.png'
+      ),
+    );
   }
 }
 
@@ -61,7 +87,20 @@ class _Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50.0, left: 10.0, right: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => CamScreen()),
+            );
+          }, child: Text("입장하기")),
+        ],
+      ),
+    );
   }
 }
 
